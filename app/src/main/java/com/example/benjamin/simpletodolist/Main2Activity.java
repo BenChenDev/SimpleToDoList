a677a5b9c8d131a056c375f4171e2359b12999af
TRUZijunChen <benjamin9527@icloud.com>
2019-01-16 16:16:45 -0800

    main 2 activity

--- app/src/main/java/com/example/benjamin/simpletodolist/Main2Activity.java
+++ app/src/main/java/com/example/benjamin/simpletodolist/Main2Activity.java
@@ -2,6 +2,9 @@ package com.example.benjamin.simpletodolist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.menu.ActionMenuItemView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        Toolbar toolbar = findViewById(R.id.toolbar2);
+        toolbar.setTitle(getString(R.string.new_task));
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_black_24dp);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/benjamin/simpletodolist/MainActivity.java
+++ app/src/main/java/com/example/benjamin/simpletodolist/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.Spinner;
 
